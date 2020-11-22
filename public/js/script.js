44de7004661e89bd6eea7774c74743f423dcc657
Junior Garcia <junior.f.garcia99@gmail.com>
2020-11-22 17:31:38 +0400

    quick update

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,35 @@
 
-console.log("hiii")
+console.log("hiii");
+console.log(document.querySelector('a-entity'));
+
+AFRAME.registerComponent("calcdistance", {
+    init: function() {
+        this.cam = document.querySelector("a-camera")
+        this.sphere = document.querySelector("a-sphere")
+    },
+    tick: function() {
+        // console.log(this.sphere);
+        // console.log(this.cam);
+        console.log('ruuuun');
+        let camPos = this.cam.object3D.position
+        let spherePos = this.sphere.object3D.position
+        let distance = camPos.distanceTo(spherePos)
+        console.log(distance);
+        if (distance < 6) {
+            // camera closer than 5m, do something
+            console.log("AAAAH change scene");
+        }
+    }
+  })
+
+
 
 
 
 /*
-    
-Tutorial Resources:
-For player movement: 
-https://www.youtube.com/watch?v=VdnN5nuxj-s
-https://github.com/saucecode/threejs-demos/blob/master/02_FloorAndMovement/demo.js 
-https://dev.to/codypearce/how-to-create-a-skybox-with-three-js-2bn8
-https://dev.to/codypearce/how-to-create-a-skybox-with-three-js-2bn8
-
-
-
-
-
+Useful Reosurces: 
+https://jsfiddle.net/jng091qx/
+https://aframe.io/docs/1.0.0/introduction/best-practices.html
 
 
 
