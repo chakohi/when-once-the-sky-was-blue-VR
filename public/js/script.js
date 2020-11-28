f53922cab4ec64abf054097ed42038b6e15723b9
slw515 <slw515@nyu.edu>
2020-11-28 15:51:21 +0400

    some different scenes

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,47 @@
-console.log("hiii");
-
+console.log(THREE);
 AFRAME.registerComponent("foo", {
   init: function() {
     this.box = document.querySelector("a-box");
-    // this.rockyTerrain = document.getElementById("rockyTerrain");
+    document.getElementById("firstPerformance").setAttribute("visible", "true");
+    document
+      .getElementById("secondPerformance")
+      .setAttribute("visible", "false");
   },
   tick: function() {
-    console.log(this.box.object3D.position);
     this.box.setAttribute("material", "color", "red");
     // this.rockyTerrain.setAttribute("position", { x: 0, y: -23, z: 0 });
   }
 });
 
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 000000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+const animate = function() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+};
+
+animate();
 /*
     
 Tutorial Resources:
