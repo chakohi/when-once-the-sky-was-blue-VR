f32be4f19f81ffb17783d36007947235226022f5
slw515 <slw515@nyu.edu>
2020-12-02 21:11:31 +0400

    small change to embers

--- public/js/francis2.js
+++ public/js/francis2.js
@@ -1,14 +1,10 @@
 console.log(" Second scene aka francis 2");
 AFRAME.registerComponent("foo", {
   init: function() {
-    this.box = document.querySelector("a-box");
-    document.getElementById("firstPerformance").setAttribute("visible", "true");
-    document
-      .getElementById("secondPerformance")
-      .setAttribute("visible", "false");
+    // this.box = document.querySelector("a-box");
   },
   tick: function() {
-    this.box.setAttribute("material", "color", "red");
+    // this.box.setAttribute("material", "color", "red");
     // this.rockyTerrain.setAttribute("position", { x: 0, y: -23, z: 0 });
   }
 });
