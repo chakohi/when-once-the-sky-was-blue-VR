e911f9cc8e1761cc07d05b1748412e8d2d9fb429
Junior Garcia <junior.f.garcia99@gmail.com>
2020-12-09 23:02:23 +0400

    finished opening credits and fixed node routing

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ const server = app.listen(process.env.PORT || port);
 app.use(express.static(path.join(__dirname, "public/")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/", "index.html"));
+  res.sendFile(path.join(__dirname, "public/", "opening.html"));
 });
+
+app.get("/lobby", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/", "lobby.html"));
+});
+app.get("/scene1", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/", "francis1.html"));
+});
+app.get("/scene2", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/", "francis2.html"));
+});
+app.get("/scene3", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/", "francis3.html"));
+});
+
+
 console.log('Server Running in Port: ',port);
