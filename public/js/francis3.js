3157251ce430ee0964b34fb31bebadaf00776ffc
slw515 <slw515@nyu.edu>
2020-12-11 16:25:39 +0400

    merge scenes into one, add third performance with sky

--- public/js/francis3.js
+++ public/js/francis3.js
@@ -1,14 +1,23 @@
 console.log("third scene aka francis 3");
+let planePos = 0;
 AFRAME.registerComponent("foo", {
   init: function() {
-    this.box = document.querySelector("a-box");
-    document.getElementById("firstPerformance").setAttribute("visible", "true");
-    document
-      .getElementById("secondPerformance")
-      .setAttribute("visible", "false");
+    this.cam = document.querySelector("a-camera");
+    this.plane = document.querySelector("#planeToFollow");
   },
-  tick: function() {
-    this.box.setAttribute("material", "color", "red");
+  tick: function(time) {
     // this.rockyTerrain.setAttribute("position", { x: 0, y: -23, z: 0 });
+    if (time < 5000) {
+    } else {
+      planePos -= 0.02;
+      console.log("planePos is: " + planePos);
+    }
+    let camPos = this.cam.object3D.position;
+
+    this.plane.setAttribute("position", {
+      x: camPos.x,
+      y: planePos,
+      z: camPos.z
+    });
   }
 });
--- public/js/opening.js
+++ public/js/opening.js
@@ -1,46 +1,38 @@
-console.log("opening title")
+console.log("opening title");
 
-$(document).ready(function () {
+$(document).ready(function() {
+  $("#lobbyMusic")
+    .get(0)
+    .play();
+  //3 seconds transition time
+  var transitiontime = 5000;
+  //first text container
+  $("#text1").fadeIn(2200);
+  setTimeout(function() {
+    $("#text2").fadeIn(2200);
+    //second textcontainer
+    setTimeout(function() {
+      $("#textcontainer1").remove();
+      $("#textcontainer2").fadeIn(2200);
 
-    //3 seconds transition time
-    var transitiontime = 5000
-    //first text container
-    $('#text1').fadeIn(2200);
-    setTimeout(function(){
-        $('#text2').fadeIn(2200);
-            //second textcontainer
-        setTimeout(function(){
-        $('#textcontainer1').remove();
-        $('#textcontainer2').fadeIn(2200);
+      setTimeout(function() {
+        $("#textcontainer2").remove();
+        $("#textcontainer3").fadeIn(2200);
 
-        setTimeout(function(){
-            $('#textcontainer2').remove();
-            $('#textcontainer3').fadeIn(2200);
+        setTimeout(function() {
+          $("#textcontainer3").remove();
+          $("#textcontainer4").fadeIn(2200);
 
-            setTimeout(function(){
+          setTimeout(function() {
+            $("#textcontainer4").remove();
+            $("#textcontainer5").fadeIn(2200);
+          }, transitiontime);
+        }, transitiontime);
+      }, transitiontime);
+    }, transitiontime);
+  }, transitiontime);
+});
 
-                $('#textcontainer3').remove();
-                $('#textcontainer4').fadeIn(2200);
-
-                setTimeout(function(){
-                    $('#textcontainer4').remove();
-                    $('#textcontainer5').fadeIn(2200);
-
-                },transitiontime)
-            },transitiontime)
-
-        },transitiontime)
-
-
-        },transitiontime);
-    
-    },transitiontime);
-
-
-
-   
-})
-
-function reload(){
-    window.location.reload(false); 
+function reload() {
+  window.location.reload(false);
 }
