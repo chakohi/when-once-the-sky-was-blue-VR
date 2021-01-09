bf3734407f261fa57d79fc9a7b3cade10255a8d0
Junior Garcia <junior.f.garcia99@gmail.com>
2021-01-09 17:19:00 +0400

    added user leaves

--- public/js/main.js
+++ public/js/main.js
@@ -32,14 +32,14 @@ AFRAME.registerComponent("foo", {
     // cameraOne.setAttribute("camera", "active: false");
     // cameraLobby.setAttribute("camera", "active: true");
 
-    // lobbyEl = document.querySelector("#lobby");
-    // scene2El = document.querySelector("#secondPerformance");
-    // scene1El = document.querySelector("#firstPerformance");
-    // scene3El = document.querySelector("#thirdPerformance");
+    lobbyEl = document.querySelector("#lobby");
+    scene2El = document.querySelector("#secondPerformance");
+    scene1El = document.querySelector("#firstPerformance");
+    scene3El = document.querySelector("#thirdPerformance");
 
     skyElementSecond = document.querySelector("#scene2Sky");
-    // skyElementFirst = document.querySelector("#scene1Sky");
-    // this.plane = document.querySelector("#planeToFollow");
+    skyElementFirst = document.querySelector("#scene1Sky");
+    this.plane = document.querySelector("#planeToFollow");
 
     scene1El.setAttribute("visible", "false");
     scene2El.setAttribute("visible", "false");
--- server.js
+++ server.js
@@ -37,11 +37,15 @@ app.get("/scene3", (req, res) => {
 var userCount = 0;
 io.sockets.on('connection', function (socket) {
   console.log('new connection: ' + socket.id);
-  console.log('yayayayay')
   userCount++;
-  console.log(userCount)
+  console.log('User Count: ',userCount)
+  socket.on('disconnect',function(){
+    console.log(' the following user just left :( ->' + socket.id);
+    userCount--;
+    console.log('User Count: ',userCount)
+  
+  })
 
 });
-
 
 console.log('Server Running in Port: ',port);
