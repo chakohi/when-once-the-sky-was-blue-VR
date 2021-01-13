f6b5a72e2c2c52e95e1457823bd5ea01edb0a228
slw515 <slw515@nyu.edu>
2021-01-13 17:07:42 +0800

    changes to interaction, use the cursor to trigger transition to scene as well as accompanying lighting change, cracks added to scene 1

--- public/js/francis2.js
+++ public/js/francis2.js
@@ -18,8 +18,10 @@ let francisTorus2Increment, francisTorus3Increment;
 let francis1;
 let currHoverFrancis;
 let scene1El;
+let cursorReticle;
 AFRAME.registerComponent("foo", {
   init: function() {
+    // cursorReticle = document.querySelector("#cursorReticle");
     // this.box = document.querySelector("a-box");
     francis1 = document.querySelector("#francis1");
     this.plane = document.querySelector("#planeToFollow");
@@ -79,17 +81,39 @@ AFRAME.registerComponent("foo", {
   tick: function(time) {
     // console.log(currHoverFrancis);
     if (state == 0) {
+      if (
+        document.querySelector("#francis1").components.light.light.intensity >
+        4.5
+      ) {
+        console.log("really intense!");
+        state = 3;
+        lobbyEl.setAttribute("visible", "false");
+        scene1El.setAttribute("visible", "true");
+        floorToFadeScene1.setAttribute("animation", "autoplay", true);
+        skyElementFirst.setAttribute("animation", "autoplay", true);
+      }
+      if (
+        document.querySelector("#francis2").components.light.light.intensity >
+        4.5
+      ) {
+        state = 2;
+        lobbyEl.setAttribute("visible", "false");
+        scene2El.setAttribute("visible", "true");
+        skyElementSecond.setAttribute("animation", "autoplay", true);
+        francisTorus2Increment = 0;
+      }
+      if (
+        document.querySelector("#francis3").components.light.light.intensity >
+        4.5
+      ) {
+        state = 4;
+        lobbyEl.setAttribute("visible", "false");
+        scene3El.setAttribute("visible", "true");
+      }
       if (currHoverFrancis == "francis1") {
-        console.log("in francis 1!");
-        console.log(
-          "should be outputting light intensity: " +
-            document.querySelector("#francis1LightTwo").components.light.light
-              .intensity
-        );
-
         if (
-          document.querySelector("#francis1LightTwo").components.light.light
-            .intensity > 5.8
+          document.querySelector("#francis1").components.light.light.intensity >
+          4.5
         ) {
           console.log("really intense!");
           state = 3;
@@ -98,6 +122,25 @@ AFRAME.registerComponent("foo", {
           floorToFadeScene1.setAttribute("animation", "autoplay", true);
           skyElementFirst.setAttribute("animation", "autoplay", true);
         }
+        // if (cursorReticle.components.material.material.opacity == 1) {
+        //   console.log("begin transition!");
+        // }
+        // console.log(
+        //   "thate length is: " +
+        //     cursorReticle.components.material.material.height
+        // );
+
+        // if (
+        //   document.querySelector("#francis1LightTwo").components.light.light
+        //     .intensity > 5.8
+        // ) {
+        //   console.log("really intense!");
+        //   state = 3;
+        //   lobbyEl.setAttribute("visible", "false");
+        //   scene1El.setAttribute("visible", "true");
+        //   floorToFadeScene1.setAttribute("animation", "autoplay", true);
+        //   skyElementFirst.setAttribute("animation", "autoplay", true);
+        // }
         // if (time % 2 == 0) {
         //   francis1Torus.setAttribute("intensity", francisTorus1Increment);
         //   francisTorus1Increment += 0.1;
@@ -241,65 +284,67 @@ AFRAME.registerComponent("foo", {
 //     });
 //   }
 // });
-AFRAME.registerComponent("raycastscript", {
-  // dependencies: ["raycaster"],
-  init: function() {
-    // this.el.addEventListener("raycaster-intersected", function(evt) {
-    //   this.raycastID = evt.srcElement.id;
-    //   currHoverFrancis = evt.srcElement.id;
-    // });
-    // this.el.addEventListener("raycaster-intersected-cleared", function(evt) {
-    //   console.log("left the object");
-    //   currHoverFrancis = null;
-    //   francis1Torus.setAttribute("visible", false);
-    //   francisTorus1Increment = 0;
-    //   francis1Torus.setAttribute("arc", `${francisTorus1Increment}`);
-    // });
-    this.el.addEventListener("hitstart", e => {
-      console.log(
-        e.target.id,
-        "collided with",
-        e.target.components["aabb-collider"]["intersectedEls"].map(x => x.id)
-      );
-      var collidedwithid =
-        e.target.components["aabb-collider"]["intersectedEls"][2].id;
-      console.log(collidedwithid);
-      if (collidedwithid == "francis1") {
-        document
-          .querySelector("#francis1LightTwo")
-          .setAttribute("animation", "autoplay", true);
-        currHoverFrancis = "francis1";
-      } else if (collidedwithid == "francis2") {
-        document
-          .querySelector("#francis2LightTwo")
-          .setAttribute("animation", "autoplay", true);
-        currHoverFrancis = "francis2";
-      } else if (collidedwithid == "francis3") {
-        document
-          .querySelector("#francis3LightTwo")
-          .setAttribute("animation", "autoplay", true);
-        currHoverFrancis = "francis3";
-      }
-    });
-  },
-  tick: function(time) {
-    if (time) {
-      let translatedPos = document
-        .querySelector("#lobbyCam")
-        .object3D.getWorldDirection();
-      // console.log(new_pos);
-      document.querySelector("#raycastCube").setAttribute("position", {
-        x:
-          document.querySelector("#lobbyCam").object3D.position.x +
-          -translatedPos.x * 2.5,
-        y:
-          document.querySelector("#lobbyCam").object3D.position.y +
-          0.7 +
-          -translatedPos.y * 1.5,
-        z:
-          document.querySelector("#lobbyCam").object3D.position.z +
-          -translatedPos.z * 2.5
-      });
-    }
-  }
-});
+// AFRAME.registerComponent("raycastscript", {
+//   // dependencies: ["raycaster"],
+//   init: function() {
+//     // this.el.addEventListener("raycaster-intersected", function(evt) {
+//     //   this.raycastID = evt.srcElement.id;
+//     //   currHoverFrancis = evt.srcElement.id;
+//     // });
+//     // this.el.addEventListener("raycaster-intersected-cleared", function(evt) {
+//     //   console.log("left the object");
+//     //   currHoverFrancis = null;
+//     //   francis1Torus.setAttribute("visible", false);
+//     //   francisTorus1Increment = 0;
+//     //   francis1Torus.setAttribute("arc", `${francisTorus1Increment}`);
+//     // });
+//     this.el.addEventListener("hitstart", e => {
+//       console.log(
+//         e.target.id,
+//         "collided with",
+//         e.target.components["aabb-collider"]["intersectedEls"].map(x => x.id)
+//       );
+//       var collidedwithid =
+//         e.target.components["aabb-collider"]["intersectedEls"][2].id;
+//       console.log(collidedwithid);
+//       if (collidedwithid == "francis1") {
+//         // document
+//         //   .querySelector("#francis1LightTwo")
+//         //   .setAttribute("animation", "autoplay", true);
+//         // cursorReticle.setAttribute("material", "opacity", "1");
+//         currHoverFrancis = "francis1";
+//       } else if (collidedwithid == "francis2") {
+//         document
+//           .querySelector("#francis2LightTwo")
+//           .setAttribute("animation", "autoplay", true);
+//         currHoverFrancis = "francis2";
+//       } else if (collidedwithid == "francis3") {
+//         document
+//           .querySelector("#francis3LightTwo")
+//           .setAttribute("animation", "autoplay", true);
+//         currHoverFrancis = "francis3";
+//       }
+//     });
+//   },
+//   tick: function(time) {
+//     // console.log(cursorReticle.components.material.material.opacity);
+//     if (time) {
+//       let translatedPos = document
+//         .querySelector("#lobbyCam")
+//         .object3D.getWorldDirection();
+//       // console.log(new_pos);
+//       document.querySelector("#raycastCube").setAttribute("position", {
+//         x:
+//           document.querySelector("#lobbyCam").object3D.position.x +
+//           -translatedPos.x * 2.5,
+//         y:
+//           document.querySelector("#lobbyCam").object3D.position.y +
+//           0.7 +
+//           -translatedPos.y * 1.5,
+//         z:
+//           document.querySelector("#lobbyCam").object3D.position.z +
+//           -translatedPos.z * 2.5
+//       });
+//     }
+//   }
+// });
