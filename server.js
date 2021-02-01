206129dbe3ee8ef3c1ccad98b76effb360b9dc38
Junior Garcia <junior.f.garcia99@gmail.com>
2021-02-01 21:29:49 +0400

    prepare for merging

--- server.js
+++ server.js
@@ -13,18 +13,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/", "opening.html"));
 });
 
-app.get("/lobby", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/", "francis2.html"));
+app.get("/performance", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/", "main.html"));
 });
-app.get("/scene1", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/", "francis1.html"));
-});
-app.get("/francis2", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/", "francis2.html"));
-});
-app.get("/scene3", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/", "francis3.html"));
-});
-
 
 console.log('Server Running in Port: ',port);
